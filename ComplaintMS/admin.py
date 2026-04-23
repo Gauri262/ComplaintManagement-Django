f7850a8b5ca952f544c1eedebf84e88fa91cc77c
Your Name <your.email@example.com>
2026-04-23 15:39:09 +0530

    removed suit from admin final fix

--- ComplaintMS/admin.py
+++ ComplaintMS/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Profile,Complaint,Grievance
-from suit.apps import DjangoSuitConfig
+# from suit.apps import DjangoSuitConfig
 
 
 class CAdmin(admin.ModelAdmin):
