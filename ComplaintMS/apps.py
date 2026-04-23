80522f51ea730114c90974c1a18323f1ea0b7a7a
Your Name <your.email@example.com>
2026-04-23 15:20:31 +0530

    final suit removal fix

--- ComplaintMS/apps.py
+++ ComplaintMS/apps.py
@@ -1,8 +1,8 @@
 from django.apps import AppConfig
-from suit.apps import DjangoSuitConfig
+# from suit.apps import DjangoSuitConfig
 
-class SuitConfig(DjangoSuitConfig):
-    layout='vertical'
+# class SuitConfig(DjangoSuitConfig):
+#     layout='vertical'
 class ComplaintMSConfig(AppConfig):
     name = 'ComplaintMS'
     
