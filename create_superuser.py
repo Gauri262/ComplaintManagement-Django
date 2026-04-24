92bb3bd0f768c228f009b9dc20c1d6f946464f38
Your Name <your.email@example.com>
2026-04-24 21:24:45 +0530

    fixed superuser script

--- create_superuser.py
+++ create_superuser.py
@@ -1,8 +1,10 @@
 import os
 import django
 
-# Setup Django
+# SET SETTINGS MODULE FIRST
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
+
+# SETUP DJANGO
 django.setup()
 
 from django.contrib.auth import get_user_model
@@ -12,18 +14,21 @@ User = get_user_model()
 username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
 password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
-if username and password:
-    user, created = User.objects.get_or_create(username=username)
+try:
+    if username and password:
+        user, created = User.objects.get_or_create(username=username)
 
-    # Always update password
-    user.set_password(password)
-    user.is_superuser = True
-    user.is_staff = True
-    user.save()
+        user.set_password(password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
 
-    if created:
-        print("Superuser created")
+        if created:
+            print("Superuser created")
+        else:
+            print("Superuser already exists, password updated")
     else:
-        print("Superuser exists, password updated")
-else:
-    print("Missing username or password")
+        print("Missing environment variables")
+
+except Exception as e:
+    print("Error creating superuser:", e)
